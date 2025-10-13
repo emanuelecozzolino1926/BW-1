3a512387437a85563d266fcd98d881b606f955aa
Leon35-fc <leon.f35@alice.it>
2025-10-13 23:52:59 +0200

    Donut fixes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,35 +1,36 @@
 //Disegno un cerchio da usare come sfondo
 
-// const circle = function {
-    const canvas = document.getElementById('donut')
-    const ctx = canvas.getContext('2d')
+const circle = function () {
+    const canvasBottom = document.getElementById('donut-bottom')
+    const ctxBottom = canvasBottom.getContext('2d')
 
-    canvas.widght = 400
-    canvas.height = 400
+    canvasBottom.widght = 400
+    canvasBottom.height = 400
 
     const radius = 100
     const angle = Math.PI / 180
 
-    ctx.beginPath()
-    ctx.strokeStyle = '#c2128d'
-    ctx.lineWidth = 20;
-    ctx.arc(150, 150, radius, angle * 0, angle * 360, false )
-    ctx.stroke()
-// }
-// circle()
+    ctxBottom.beginPath()
+    ctxBottom.strokeStyle = '#c2128d'
+    ctxBottom.lineWidth = 20;
+    ctxBottom.arc(150, 150, radius, angle * 0, angle * 360, false )
+    ctxBottom.stroke()    
+}
+
+circle()
 
 //Disegno un cerchio che indicherà la percentuale delle risposte
-//  const canvas = document.getElementById('donut')
-//     const ctx = canvas.getContext('2d')
+    const canvasTop = document.getElementById('donut-top')
+    const ctxTop = canvasTop.getContext('2d')
 
-//     canvas.widght = 400
-//     canvas.height = 400
+    canvasTop.widght = 400
+    canvasTop.height = 400
 
-//     const radius = 100
-//     const angle = Math.PI / 180
+    const radius = 100
+    const angle = Math.PI / 180
 
-//     ctx.beginPath()
-//     ctx.strokeStyle = '#00ffff'
-//     ctx.lineWidth = 20;
-//     ctx.arc(150, 150, radius, angle * 0, angle * 270, false )
-//     ctx.stroke()
+    ctxTop.beginPath()
+    ctxTop.strokeStyle = '#00ffff'
+    ctxTop.lineWidth = 20;
+    ctxTop.arc(150, 150, radius, angle * 0, angle * 270, false )
+    ctxTop.stroke()
